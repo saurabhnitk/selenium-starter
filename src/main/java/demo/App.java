0fa624c984c8b7ece570736bfe6d7592b98c1658
Saurabh Kumar <saurabhnitk13@gmail.com>
2023-08-21 22:51:56 +0530

    commit 3

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -8,15 +8,17 @@ import java.net.MalformedURLException;
 public class App {
     public void getGreeting() throws InterruptedException, MalformedURLException {
         //TestCases tests = new TestCases(); // Initialize your test class
-        SearchAmazon search = new SearchAmazon();
+        //SearchAmazon search = new SearchAmazon();
+        BookMyShow links = new BookMyShow();
         //TODO: call your test case functions one after other here
 
         //tests.testCase01();
-        search.testCase01();
-
+        //search.testCase01();
+        links.testCase01();
         //END Tests
         //tests.endTest(); // End your test by clearning connections and closing browser
-        search.endTest();
+        //search.endTest();
+        links.endTest();
     }
 
     public static void main(String[] args) throws InterruptedException, MalformedURLException {
